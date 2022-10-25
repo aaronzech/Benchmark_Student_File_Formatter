60aae0fdddd24060f455988136ca69a649c0cc4b
Aaron Zech <aaronzech@gmail.com>
2022-10-25 11:40:46 -0500

    Add GUI file picker

--- benchmark_student_file.py
+++ benchmark_student_file.py
@@ -3,11 +3,25 @@ import pandas as pd
 from openpyxl import Workbook,load_workbook
 from openpyxl.utils import get_column_letter
 
-student_file = 'students.xlsx'
+#student_file = 'students.xlsx'
+#clever_file = 'clever.xlsx'
+
+import tkinter as tk
+from tkinter import filedialog
+root = tk.Tk()
+root.withdraw()
+print("Select student file")
+student_file = filedialog.askopenfilename()
+print("Select clever file")
+clever_file = filedialog.askopenfilename()
+
+#convert to XLSX format
+clever_file = pd.read_csv (clever_file)
+clever_file.to_excel (r'clever.xlsx', index = None, header=True)
 clever_file = 'clever.xlsx'
 
-wb = load_workbook(student_file) #Load Workbook
-ws = wb.active #Worksheet
+
+
 #print(ws)
 
 #print(ws['A1'].value) # Read value at A1
@@ -22,12 +36,15 @@ ws = wb.active #Worksheet
 
 
 # Change the Kindergarten Grade code from KF or 25 to K
-def fixKindergartenGradeLevel():
+def fixKindergartenGradeLevel(file):
+    wb = load_workbook(file) #Load Workbook
+    ws = wb.active #Worksheet
     for row in range (1,900): #stops at row 900
         for col in range (10,11): # column J
             char = get_column_letter(col)
             if ws[char + str(row)].value == "KF" or ws[char + str(row)].value == "25":
                 ws[char + str(row)].value = 'K'
+    wb.save(file)
             
 
 def formatCleverFile(file):
@@ -40,7 +57,8 @@ def mergeSheets(student_file,clever_file):
     #merging data frames with merge function in pandas
     # load in the various tables from an excel document
     studentData = pd.read_excel(student_file,sheet_name='QRY801')
-    cleverData = pd.read_excel(clever_file,sheet_name='clever') # needs modification
+    #cleverData = pd.read_excel(clever_file,sheet_name='clever') # needs modification
+    cleverData = pd.read_excel(clever_file,sheet_name='Sheet1')
     merge = studentData.merge(cleverData, on="Student's SIS Id", how='left')
     #print(merge)
     #merge.to_csv('merge.csv',index=False)
@@ -58,7 +76,7 @@ def formatSheet(file):
     for row in range (1,2): #stops at row 10
      for col in range (11,15): #columns 1 -4
          char = get_column_letter(col)
-         print(ws[char + str(row)].value)
+         #print(ws[char + str(row)].value)
     
     #Move Column M to Column K
     for row in range (1,900): #stops at row 10
@@ -66,7 +84,7 @@ def formatSheet(file):
         char = get_column_letter(col)
        
         ws[char + str(row)].value = ws[get_column_letter(13) + str(row)].value
-        print(ws[char + str(row)].value)
+        #print(ws[char + str(row)].value)
     # change colmun header
     ws['K1'].value = 'SSO Id'
     ws.delete_cols(13) 
@@ -74,8 +92,12 @@ def formatSheet(file):
 
 
 #Main Program 
-fixKindergartenGradeLevel()
-formatCleverFile(clever_file)
-wb.save(student_file)
-mergeSheets(student_file,clever_file)
-formatSheet('merge.xlsx')
+
+if __name__ == "__main__":
+    print("Starting...")
+    fixKindergartenGradeLevel(student_file)
+    formatCleverFile(clever_file)
+    
+    mergeSheets(student_file,clever_file)
+    formatSheet('merge.xlsx')
+    print("finished")
